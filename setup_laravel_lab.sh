ced5117d0b3c350d74b9869431c25e7c3807cb9b
consultai <carlitotate12160@gmail.com>
2026-06-22 14:58:45 +0700

    Fix setup_laravel_lab.sh: use --no-security-blocking instead of --ignore-advisories

--- setup_laravel_lab.sh
+++ setup_laravel_lab.sh
@@ -29,7 +29,7 @@ if [ -d "lab-target" ]; then
     rm -rf lab-target
 fi
 
-/usr/local/bin/composer create-project laravel/laravel lab-target --no-interaction --ignore-platform-reqs --ignore-advisories
+/usr/local/bin/composer create-project laravel/laravel lab-target --no-interaction --ignore-platform-reqs --no-security-blocking
 cd lab-target
 
 # Enable debug mode
